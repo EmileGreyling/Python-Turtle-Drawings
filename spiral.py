51cdae14ad807b3d21a80c1cc68a0a4bcef3ef01
Emile Greyling <coding-hacks@localhost.localdomain>
2022-11-18 19:11:33 +0200

    Added spiral art

---
+++ spiral.py
@@ -0,0 +1,20 @@
+# Kaleido-spiral
+
+from turtle import *
+from itertools import cycle
+
+colors = cycle(['red', 'orange', 'yellow', \
+	'green', 'blue', 'purple'])
+
+def draw_circle(size, angle, shift):
+	color(next(colors))
+	circle(size)
+	right(angle)
+	forward(shift)
+	draw_circle(size + 2, angle + 2, shift + .5)
+
+hideturtle()
+bgcolor('black')
+speed(15)
+pensize(4)
+draw_circle(30, 0, 1)
