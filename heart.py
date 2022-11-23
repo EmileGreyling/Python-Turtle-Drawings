8548cf76cc8ae8a4cba861c06f0d421cbe4668cd
Emile Greyling <coding-hacks@localhost.localdomain>
2022-11-23 12:40:27 +0200

    Added More Drawings

---
+++ heart.py
@@ -0,0 +1,27 @@
+# How To Impress your crush with Python
+
+
+from turtle import *
+
+# Set background color to black
+bgcolor("black")
+
+# Set turtle color to read
+color("red")
+
+begin_fill()
+pensize(3)
+
+# Start Drawing
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
+
+# Hide the turtle
+hideturtle()
+
+done()
--- spiral.py
+++ spiral.py
@@ -1,17 +1,16 @@
-# Kaleido-spiral
-
 from turtle import *
 from itertools import cycle
 
-colors = cycle(['red', 'orange', 'yellow', \
-	'green', 'blue', 'purple'])
+colors = cycle(['red', 'orange', \
+	'yellow', 'green', 'blue', 'purple'])
 
 def draw_circle(size, angle, shift):
 	color(next(colors))
 	circle(size)
 	right(angle)
 	forward(shift)
-	draw_circle(size + 2, angle + 2, shift + .5)
+	draw_circle(size + 2, angle + 2, \
+		shift + .5)
 
 hideturtle()
 bgcolor('black')
